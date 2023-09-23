efde116387c8ef99423512ebee66ff35e998ef07
sebastian20210401 <sebastian20210401@gmail.com>
2023-09-23 12:42:39 +0800

    The Character Class

---
+++ Source/Aura/Private/Characters/AuraCharacterBase.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+// Called when the game starts or when spawned
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
